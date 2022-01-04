8eb78d3ac93e718f141e12acf1da74ae2452a5c2
onniseonmi <soul012632@gmail.com>
2022-01-04 15:42:41 +0800

    Moved files

--- client/src/features/MyImage.js/IntroPage.js
+++ client/src/features/MyImage.js/IntroPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import seonmi from '../../Images/choi.jpg';
+import styles from './IntroPage.module.css';
 
 export default function IntroPage() {
   return (
-    <div className='myImage'>
-      <img src={seonmi} alt='logo'></img>
+    <div className={styles.myImage}>
+      <img className={styles.image} src={seonmi} alt='logo'></img>
       <Link to='/'>Home</Link>
     </div>
   );
